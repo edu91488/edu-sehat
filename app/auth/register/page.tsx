d5309128c18bf4640ca4cad810a3b91ede24bbe8
Ariya Ganteng <ariyahandika04@gmail.com>
2026-02-03 02:17:10 +0700

    jarak button login dan regis

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -147,7 +147,7 @@ export default function RegisterPage() {
                 />
               </div>
             </CardContent>
-            <CardFooter className="flex flex-col gap-4">
+            <CardFooter className="flex flex-col gap-4 mt-8">
               <Button type="submit" className="w-full" disabled={isLoading}>
                 {isLoading ? (
                   <>
